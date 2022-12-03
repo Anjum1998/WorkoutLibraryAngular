1401d69d55925817a25aab9f4b70f6e5ff126657
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-03 21:48:02 +0530

    view

---
+++ src/app/view-book/view-book.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-book',
+  templateUrl: './view-book.component.html',
+  styleUrls: ['./view-book.component.css']
+})
+export class ViewBookComponent {
+
+  book:any=[
+    {
+      "title":"Shard",
+      "image":"https://images-platform.99static.com//vWBHU7wg6B3gcON8tK4YyLYHJ44=/140x103:961x924/fit-in/500x500/projects-files/117/11772/1177210/4cd9a894-b6bb-4e13-be28-a0ff4da21a02.jpeg",
+      "author":"James R Benn"
+    },
+    {
+      "title":"Shard",
+      "image":"https://images-platform.99static.com//vWBHU7wg6B3gcON8tK4YyLYHJ44=/140x103:961x924/fit-in/500x500/projects-files/117/11772/1177210/4cd9a894-b6bb-4e13-be28-a0ff4da21a02.jpeg",
+      "author":"James R Benn"
+    },
+    {
+      "title":"Shard",
+      "image":"https://images-platform.99static.com//vWBHU7wg6B3gcON8tK4YyLYHJ44=/140x103:961x924/fit-in/500x500/projects-files/117/11772/1177210/4cd9a894-b6bb-4e13-be28-a0ff4da21a02.jpeg",
+      "author":"James R Benn"
+    },
+    {
+      "title":"Shard",
+      "image":"https://images-platform.99static.com//vWBHU7wg6B3gcON8tK4YyLYHJ44=/140x103:961x924/fit-in/500x500/projects-files/117/11772/1177210/4cd9a894-b6bb-4e13-be28-a0ff4da21a02.jpeg",
+      "author":"James R Benn"
+    }
+    
+  ]
+
+}
