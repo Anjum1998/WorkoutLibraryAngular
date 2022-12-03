c66730c8a27327b9817511d9a2f2020f20fc665c
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-03 19:42:50 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminLoginComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,8 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
